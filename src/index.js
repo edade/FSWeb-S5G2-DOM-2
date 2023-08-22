55ec830d1d2662cafa64bd6ad0364499b09b941d
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-08-22 22:42:06 +0300

    click ve dblclick özellikleri kullanıldı

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ document.querySelector("h1").addEventListener("click", function (e) {
 
 // Kodlar buraya gelecek!
 
-const nav = document.getElementsByClassName("nav-link");
+const nav = document.getElementsByClassName("nav-link")[0];
 
-/*nav.addEventListener("mouseover", (event) => {
-    nav.classList.add()
-});*/
+nav.addEventListener("dblclick", (event) => {
+  alert("Zaten Anasayfadasınız :)");
+});
 
 //Güneş altında eğlence butonu için
 const button = document.getElementsByClassName("btn")[0];
@@ -33,12 +33,13 @@ button2.addEventListener("click", (event) => {
 document.addEventListener("keypress", (event) => {
   event.preventDefault();
 });
-
+// Haydi gidelim kısmını wheel ile italic yapma
 const text = document.getElementsByClassName("text-content")[0];
 
 text.addEventListener("wheel", (event) => {
   event.target.style.fontStyle = "italic";
 });
+//Macera beklemez kısmında wheel ile font değiştirme
 const text2 = document.getElementsByClassName("text-content")[1];
 
 text2.addEventListener("wheel", (event) => {
